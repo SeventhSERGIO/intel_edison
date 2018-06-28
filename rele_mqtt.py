0bf5fbe4a0735afaa0246f29c4d9dfad9c908ff9
Adrian Martinez <adrianmartinez@Adrians-MacBook-Pro.local>
2018-06-28 10:05:24 -0600

    Adrian
    m

--- rele_mqtt.py
+++ rele_mqtt.py
@@ -11,10 +11,8 @@ gpio_1.dir(mraa.DIR_OUT)
 def on_message_print(client, userdata, message):
     print("%s %s" % (message.topic, message.payload))
     if message.payload == "ON":
-		gpio_1.write(1)
-		#print("Hola")
-	elif message.payload == "OFF":
-		gpio_1.write(0)
-		#print("Adios")
+	   gpio_1.write(1)
+    elif message.payload == "OFF":
+	   gpio_1.write(0)
 
 subscribe.callback(on_message_print, "topico/ejemplo", hostname="192.168.1.108")
