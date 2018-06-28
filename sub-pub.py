178c115778576b635455bcdb8e9c6451023048ea
Adrian Martinez <adrianmartinez@Adrians-MacBook-Pro.local>
2018-06-28 13:07:17 -0600

    Adrian
    m

---
+++ sub-pub.py
@@ -0,0 +1,24 @@
+import mraa
+import sys
+import time
+import paho.mqtt.subscribe as subscribe
+import paho.mqtt.publish as publish
+
+# initialise gpio 2
+gpio_1 = mraa.Gpio(2)
+# set gpio 2 to output
+gpio_1.dir(mraa.DIR_OUT)
+pot = mraa.Aio(0) 
+
+def on_message_print(client, userdata, message):
+    print("%s %s" % (message.topic, message.payload))
+    if message.payload == "ON":
+	   gpio_1.write(1)
+    elif message.payload == "OFF":
+	   gpio_1.write(0)
+
+subscribe.callback(on_message_print, "topico/ejemplo_1", hostname="192.168.1.108")
+
+while 1:
+    potVal = float(pot.read())
+    publish.single("topico/relay_1", str(potVal), hostname="192.168.1.108")
