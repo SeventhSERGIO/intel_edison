5272fcdf10498362819ea79cf93b6a8022b97a4b
Adrian Martinez <adrianmartinez@Adrians-MacBook-Pro.local>
2018-06-28 09:41:09 -0600

    Adrian
    m

---
+++ rele_mqtt.py
@@ -0,0 +1,18 @@
+import mraa
+import time
+import paho.mqtt.subscribe as subscribe
+
+# initialise gpio 2
+gpio_1 = mraa.Gpio(2)
+
+# set gpio 2 to output
+gpio_1.dir(mraa.DIR_OUT)
+
+def on_message_print(client, userdata, message):
+    print("%s %s" % (message.topic, message.payload))
+    if message.topic == "ON"
+    	gpio_1.write(1)
+    else if message.topic == "OFF"
+    	gpio_1.write(0)
+
+subscribe.callback(on_message_print, "topico/ejemplo", hostname="192.168.1.130")
