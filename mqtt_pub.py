9593da0236312180221860927dad3d0589cc90a8
Adrian Martinez <adrianmartinez@Adrians-MacBook-Pro.local>
2018-06-28 13:41:01 -0600

    Adrian
    m

--- mqtt_pub.py
+++ mqtt_pub.py
@@ -3,18 +3,18 @@ import mraa
 import sys
 import time
 
-def on_subscribe(client, userdata, mid, granted_qos):
+def on_subscribe(client1, userdata, mid, granted_qos):
     print("Subscribed: "+str(mid)+" "+str(granted_qos))
 
-def on_message(client, userdata, msg):
+def on_message(client1, userdata, msg):
     print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))    
 
 
 broker="192.168.1.108"
 port=1883
 client1= paho.Client("Edison_Adrian")   
-client.on_subscribe = on_subscribe
-client.on_message = on_message                                              
+client1.on_subscribe = on_subscribe
+client1.on_message = on_message                                              
 client1.connect(broker,port)    
 client1.subscribe("topico/relay_1",0)                             
 # Publish
