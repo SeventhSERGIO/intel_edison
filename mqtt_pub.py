839bcf360f6369ddb653e06df128355025191c66
Adrian Martinez <adrianmartinez@Adrians-MacBook-Pro.local>
2018-06-28 01:38:51 -0600

    Adrian
    m

--- mqtt_pub.py
+++ mqtt_pub.py
@@ -1,6 +1,6 @@
 import paho.mqtt.client as paho
-broker="192.168.1.100"
-port=2000
+broker="192.168.1.130"
+port=1883
 # Create function for callback
 def on_publish(client,userdata,result):             
         print("data published \n")
@@ -12,4 +12,4 @@ client1.on_publish = on_publish
 # Establish connection                        
 client1.connect(broker,port)                                 
 # Publish
-ret= client1.publish("casa/puerta_principal/set","Hola")                   
+ret= client1.publish("topico/ejemplo","Hola")                   
