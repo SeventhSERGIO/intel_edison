861f8ce6223bf747b875286658cfae99c70a028f
Adrian Martinez <adrianmartinez@Adrians-MacBook-Pro.local>
2018-06-28 01:59:45 -0600

    Adrian
    m

--- sub.py
+++ sub.py
@@ -1,24 +1,6 @@
-import paho.mqtt.client as mqtt #import the client1
-import time
-############
-def on_message(client, userdata, message):
-    print("message received " ,str(message.payload.decode("utf-8")))
-    print("message topic=",message.topic)
-    print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
-########################################
-while 1
-	broker_address="192.168.1.130"
-	#broker_address="iot.eclipse.org"
-	print("creating new instance")
-	client = mqtt.Client("P1") #create new instance
-	client.on_message=on_message #attach function to callback
-	print("connecting to broker")
-	client.connect(broker_address) #connect to broker
-	client.loop_start() #start the loop
-	print("Subscribing to topic","topico/ejemplo")
-	client.subscribe("topico/ejemplo")
-#print("Publishing message to topic","house/bulbs/bulb1")
-#client.publish("house/bulbs/bulb1","OFF")
-#time.sleep(4) # wait
-#client.loop_stop() #stop the loop
+import paho.mqtt.subscribe as subscribe
+
+def on_message_print(client, userdata, message):
+    print("%s %s" % (message.topic, message.payload))
+
+subscribe.callback(on_message_print, "topico/ejemplo", hostname="192.168.1.130")
