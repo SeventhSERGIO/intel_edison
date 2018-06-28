d8958a7f5348d99d997cf49d84263ff4ee7b5395
Adrian Martinez <adrianmartinez@Adrians-MacBook-Pro.local>
2018-06-28 01:52:14 -0600

    Adrian
    m

--- sub.py
+++ sub.py
@@ -7,17 +7,17 @@ def on_message(client, userdata, message):
     print("message qos=",message.qos)
     print("message retain flag=",message.retain)
 ########################################
-broker_address="192.168.1.130"
-#broker_address="iot.eclipse.org"
-print("creating new instance")
-client = mqtt.Client("P1") #create new instance
-client.on_message=on_message #attach function to callback
-print("connecting to broker")
-client.connect(broker_address) #connect to broker
-client.loop_start() #start the loop
-print("Subscribing to topic","topico/ejemplo")
-client.subscribe("topico/ejemplo")
-client.loop_forever()
+while (1)
+	broker_address="192.168.1.130"
+	#broker_address="iot.eclipse.org"
+	print("creating new instance")
+	client = mqtt.Client("P1") #create new instance
+	client.on_message=on_message #attach function to callback
+	print("connecting to broker")
+	client.connect(broker_address) #connect to broker
+	client.loop_start() #start the loop
+	print("Subscribing to topic","topico/ejemplo")
+	client.subscribe("topico/ejemplo")
 #print("Publishing message to topic","house/bulbs/bulb1")
 #client.publish("house/bulbs/bulb1","OFF")
 #time.sleep(4) # wait
